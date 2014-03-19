6deeb5e72fbe9d048bc264fbe71e13a1c6517887
naconn05 <naconn05@louisville.edu>
2014-03-19 17:22:12 -0400

    Assignment 6
    Incomplete PetStore

---
+++ PetStore/PetStore/Controllers/PetsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PetStore.Models;
+
+namespace PetStore.Controllers
+{
+    public class PetsController : Controller
+    {
+        private PetDBContext db = new PetDBContext();
+
+        // GET: /Pets/
+        public ActionResult Index(string searchString)
+        {
+            var pets = from m in db.Pets
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                pets = pets.Where(s => s.Name.Contains(searchString));
+            }
+
+            return View(pets);
+        }
+
+        // GET: /Pets/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pets pets = db.Pets.Find(id);
+            if (pets == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pets);
+        }
+
+        // GET: /Pets/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Pets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,Name,Description,DateReceived,Quantity,Price")] Pets pets)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pets.Add(pets);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pets);
+        }
+
+        // GET: /Pets/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pets pets = db.Pets.Find(id);
+            if (pets == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pets);
+        }
+
+        // POST: /Pets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,Name,Description,DateReceived,Quantity,Price")] Pets pets)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pets).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pets);
+        }
+
+        // GET: /Pets/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pets pets = db.Pets.Find(id);
+            if (pets == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pets);
+        }
+
+        // POST: /Pets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pets pets = db.Pets.Find(id);
+            db.Pets.Remove(pets);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PetStore/PetStore/Models/Pets.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace PetStore.Models
+{
+    public class Pets
+    {
+        public int ID { get; set; }
+        public string Name { get; set;}
+        public string Description { get; set; }
+        public DateTime DateReceived { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class PetDBContext : DbContext
+    {
+        public DbSet<Pets> Pets { get; set; }
+    }
+}
